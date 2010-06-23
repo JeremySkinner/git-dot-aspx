6d22e5da81e9776e2453b5aa6bdc1833671c0771
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-06-23 09:36:02 +0100

    Tweak BaseTest to change the web.config

--- GitAspx.Tests/BaseTest.cs
+++ GitAspx.Tests/BaseTest.cs
@@ -1,8 +1,11 @@
 namespace GitAspx.Tests {
 	using System;
 	using System.IO;
+	using System.Web.Configuration;
+	using System.Xml.Linq;
 	using CassiniDev;
 	using NUnit.Framework;
+	using System.Linq;
 
 	[TestFixture]
 	public class BaseTest : CassiniDevServer {
@@ -10,13 +13,25 @@ namespace GitAspx.Tests {
 
 		[TestFixtureSetUp]
 		public void TestFixtureSetup() {
+			ChangeRepo(new DirectoryInfo("..\\..\\Repositories").FullName);
 			StartServer("..\\..\\..\\GitAspx");
 		}
 
 		[TestFixtureTearDown]
 		public void TestFixtureTeardown() {
 			StopServer();
+			ChangeRepo("C:\\Repositories");
 		}
 
+		private void ChangeRepo(string repoDir) {
+			string path = "..\\..\\..\\GitAspx\\Web.config";
+
+			var doc = XDocument.Load(path);
+			doc.Element("configuration").Element("appSettings").Elements("add").Single(
+				x => x.Attribute("key").Value == "RepositoriesDirectory")
+				.Attribute("value").Value = repoDir;
+
+			doc.Save(path);
+		}
 	}
 }
