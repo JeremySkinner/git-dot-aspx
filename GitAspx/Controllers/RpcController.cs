5e64b173f40641bb05164321ab7ecaa75a61d1c4
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-06-25 15:04:48 +0100

    Remove some duplication in RpcController

--- GitAspx/Controllers/RpcController.cs
+++ GitAspx/Controllers/RpcController.cs
@@ -3,6 +3,7 @@ namespace GitAspx.Controllers {
 	using System.IO;
 	using System.Web.Mvc;
 	using GitAspx.Lib;
+	using GitSharp.Core;
 	using GitSharp.Core.Transport;
 
 	// Handles project/git-upload-pack and project/git-receive-pack
@@ -14,47 +15,38 @@ namespace GitAspx.Controllers {
 		}
 
 		public ActionResult UploadPack(string project) {
-			if(!HasAccess(Rpc.UploadPack, checkContentType: true)) {
-				return new ForbiddenResult();
-			}
-
-			Response.ContentType = "application/x-git-upload-pack-result";
-			WriteNoCache();
-
-			var repository = repositories.GetRepository(project);
-
-			if(repository == null) {
-				return new NotFoundResult();
-			}
-
-			using (repository) {
+			return ExecuteRpc(project, Rpc.UploadPack, repository => {
 				using (var pack = new UploadPack(repository)) {
 					pack.setBiDirectionalPipe(false);
 					pack.Upload(Request.InputStream, Response.OutputStream, Response.OutputStream);
 				}
-			}
-
-			return new EmptyResult();
+			});
 		}
 
 		public ActionResult ReceivePack(string project) {
-			if (!HasAccess(Rpc.ReceivePack, checkContentType: true)) {
+			return ExecuteRpc(project, Rpc.ReceivePack, repository => {
+				var pack = new ReceivePack(repository);
+				pack.setBiDirectionalPipe(false);
+				pack.receive(Request.InputStream, Response.OutputStream, Response.OutputStream);
+			});
+		}
+
+		private ActionResult ExecuteRpc(string project, Rpc rpc, Action<Repository> action) {
+			if (!HasAccess(Rpc.UploadPack, checkContentType: true)) {
 				return new ForbiddenResult();
 			}
 
-			Response.ContentType = "application/x-git-receive-pack-result";
+			Response.ContentType = string.Format("application/x-git-{0}-result", rpc.GetDescription());
 			WriteNoCache();
 
 			var repository = repositories.GetRepository(project);
 
-			if(repository == null) {
+			if (repository == null) {
 				return new NotFoundResult();
 			}
 
 			using (repository) {
-				var pack = new ReceivePack(repository);
-				pack.setBiDirectionalPipe(false);
-				pack.receive(Request.InputStream, Response.OutputStream, Response.OutputStream);
+				action(repository);
 			}
 
 			return new EmptyResult();
